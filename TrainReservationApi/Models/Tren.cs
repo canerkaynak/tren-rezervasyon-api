68b06c8c9d1706b1e5bc84448edd577dc32bf6b7
canerkaynak <77839466+canerkaynak@users.noreply.github.com>
2024-09-27 21:11:30 +0300

    Update Tren.cs

--- TrainReservationApi/Models/Tren.cs
+++ TrainReservationApi/Models/Tren.cs
@@ -3,6 +3,6 @@
     public class Tren
     {
         public required string Ad { get; set; } = string.Empty;
-        public required List<Vagon> Vagonlar { get; set; } = new List<Vagon>();
+        public required List<Vagon> Vagonlar { get; set; }
     }
 }
