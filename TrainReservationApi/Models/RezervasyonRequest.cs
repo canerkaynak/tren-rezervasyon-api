e4e675f38f8895d761a0ac13a6e6f2d69033a717
canerkaynak <77839466+canerkaynak@users.noreply.github.com>
2024-09-27 21:11:08 +0300

    Update RezervasyonRequest.cs

--- TrainReservationApi/Models/RezervasyonRequest.cs
+++ TrainReservationApi/Models/RezervasyonRequest.cs
@@ -2,7 +2,7 @@
 {
     public class RezervasyonRequest
     {
-        public required Tren Tren { get; set; } = new Tren();
+        public required Tren Tren { get; set; }
         public required int RezervasyonYapilacakKisiSayisi { get; set; }
         public required bool KisilerFarkliVagonlaraYerlestirilebilir { get; set; }
     }
