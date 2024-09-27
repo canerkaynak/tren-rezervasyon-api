43eee5b8fe30c1eb136ba4732f4b4c433514e923
canerkaynak <77839466+canerkaynak@users.noreply.github.com>
2024-09-27 21:04:14 +0300

    Update RezervasyonResponse.cs

--- TrainReservationApi/Models/RezervasyonResponse.cs
+++ TrainReservationApi/Models/RezervasyonResponse.cs
@@ -3,12 +3,12 @@
     public class RezervasyonResponse
     {
         public bool RezervasyonYapilabilir { get; set; }
-        public List<YerlesimAyrinti> YerlesimAyrinti { get; set; }
+        public List<YerlesimAyrinti> YerlesimAyrinti { get; set; } = new List<YerlesimAyrinti>();
     }
 
     public class YerlesimAyrinti
     {
-        public string VagonAdi { get; set; }
+        public string VagonAdi { get; set; } = string.Empty;
         public int KisiSayisi { get; set; }
     }
 }
